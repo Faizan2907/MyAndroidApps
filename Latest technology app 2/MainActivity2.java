314f270175da5aecdfb4c6feb937fb53c668ba71
FAIZAN ALI IQBAL ALI <67515374+Faizan2907@users.noreply.github.com>
2023-01-03 02:02:11 +0530

    Add files via upload

---
+++ Latest technology app 2/MainActivity2.java
@@ -0,0 +1,37 @@
+package com.example.mygalleryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        myWebView = findViewById(R.id.webView);
+
+        String url = getIntent().getStringExtra("url");
+        myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl(url);
+    }
+
+    public void onBackPressed(){
+        if(myWebView.canGoBack()){
+            myWebView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
