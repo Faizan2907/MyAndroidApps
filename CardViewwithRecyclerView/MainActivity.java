1be0d5f8bee39a8c2bbb6f73255755aa89233757
FAIZAN ALI IQBAL ALI <67515374+Faizan2907@users.noreply.github.com>
2023-01-09 23:36:54 +0530

    Add files via upload

---
+++ CardViewwithRecyclerView/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.cardviewwithrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private List<String> titles;
+    private List<Integer> mImages;
+
+    private MyAdapter myAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        titles = new ArrayList<>();
+        mImages = new ArrayList<>();
+
+        myAdapter = new MyAdapter(this, titles, mImages);
+
+        mImages.add(R.drawable.c);
+        mImages.add(R.drawable.cpp);
+        mImages.add(R.drawable.java);
+        mImages.add(R.drawable.css);
+        mImages.add(R.drawable.csharp);
+        mImages.add(R.drawable.angular);
+        mImages.add(R.drawable.html);
+
+        titles.add("C Programming");
+        titles.add("C++ Programming");
+        titles.add("Java Programming");
+        titles.add("Cascading Style Sheets");
+        titles.add("C Sharp Programming");
+        titles.add("Angular Programming");
+        titles.add("HTML");
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecyclerView.setAdapter(myAdapter);
+    }
+}
---
+++ CardViewwithRecyclerView/MyAdapter.java
@@ -0,0 +1,61 @@
+package com.example.cardviewwithrecyclerview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+
+    private Context context;
+    private List<String> title;
+    private List<Integer> images;
+
+    public MyAdapter(Context context, List<String> title, List<Integer> images){
+        this.context = context;
+        this.title = title;
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.grid_item, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.mTextView.setText(title.get(position));
+        holder.mImageView.setImageResource(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return title.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView mImageView;
+        TextView mTextView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mImageView = itemView.findViewById(R.id.imageView);
+            mTextView =  itemView.findViewById(R.id.textView);
+        }
+    }
+
+
+}
